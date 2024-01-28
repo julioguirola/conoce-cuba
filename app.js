7ffa97481791e5b195a1037414ff6aef6ac4d2ef
julioguirola <julioguirola03@gmail.com>
2024-01-28 12:50:55 -0500

    finalizados+

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express, { json } from "express";
+import express from "express";
 import { getProyecto, getProyectos } from "./db_connect/db.js";
 
 const app = express();
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.json())
 app.use(express.static('./frontend'))
 app.use(express.static('./frontend/fotos'))
+app.set('view engine', 'ejs');
 
 app.get('/', (req,res) => {
     res.sendFile(process.cwd() + '/frontend/index.html')
@@ -31,8 +32,14 @@ app.post('/get_data', async (req,res) => {
 
 app.get('/terminados/:id', async (req,res) => {
     const {id} = req.params
+    const {nombre} = req.query
     const result = await getProyecto(id)
-    res.json(result)
+
+    res.render("terminado.ejs", {
+        datos:result,
+        proyectoNombre: nombre
+    })
+
 })
 
 app.get('/proyectos/:id', (req,res) => {
@@ -43,4 +50,4 @@ app.listen(8080 , () => {
     console.log("http://localhost:8080")
 })
 
-//token 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
+//token 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
